01a905649c569de73d76f829daf4b114ebd4c355
Abdulrahman Mohammed <aboodsamt84@gmail.com>
2026-07-23 13:40:58 +0300

    make the total revenue and top five custmers functions

--- main.dart
+++ main.dart
@@ -3,4 +3,6 @@ import 'orders_functions.dart';
 
 void main() {
   print(total_revenue_per_city(orders));
+  print('///////////////////////////////');
+  print(top_five_custmers(orders));
 }
--- orders_functions.dart
+++ orders_functions.dart
@@ -1,5 +1,22 @@
 Map<String, dynamic> total_revenue_per_city(List<Map<String, dynamic>> orders) {
   Map<String, dynamic> revenue_per_city = {};
-  orders.map((e) => revenue_per_city[e['city']] += e['total']);
+  orders.map((e) {
+    revenue_per_city.containsKey(e['city'])
+        ? revenue_per_city[e['city']] += e['total']
+        : revenue_per_city[e['city']] = e['total'];
+  }).toList();
   return revenue_per_city;
+}
+
+List<String> top_five_custmers(List<Map<String, dynamic>> orders) {
+  Map<String, dynamic> total_custmers = {};
+  orders.map((e) {
+    total_custmers.containsKey(e['customer'])
+        ? total_custmers[e['customer']] += e['total']
+        : total_custmers[e['customer']] = e['total'];
+  }).toList();
+  var sorted_total_custmers = total_custmers.entries.toList()
+    ..sort((a, b) => b.value.compareTo(a.value));
+  List<String> top_five_custmers = sorted_total_custmers.take(5).map((e) => e.key).toList();
+  return top_five_custmers;
 }
